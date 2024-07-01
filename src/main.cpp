5141f7da3cfac904cbd6198e0bb90f338c14533b
David Apanco <dperezapanco@gmail.com>
2024-07-01 01:28:15 -0600

    exit 0

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,74 @@
 #include <iostream>
+#include <sstream>
+#include <string>
+
 using namespace std;
 
 void start();
+string getCommand();
+string cleanCommand( string& str );
+string getCode(string& str);
+
+//utils
+
+int stringToInt(string str);
 
 int main() {
 	// Flush after every std::cout / std:cerr
 	cout << unitbuf; // Asegurar que cout no esté almacenado en búfer
 	cerr << unitbuf; // Asegurar que cerr no esté almacenado en búfer
 	// Uncomment this block to pass the first stage
-	
 	while(true){
 		start();
 	}
 }
 
 void start(){
-	cout << "$ ";
-	  
+	string input, command, code;
+	input = getCommand();
+	
+	command = cleanCommand(input);
+	code = getCode(input);
+	//Comandos identificados
+	
+	if(command == "exit"){
+		int codeInt = stringToInt(code);
+		exit(codeInt);
+	}
+	
+	//Comando no identificado
+	else{
+		cout << input << ": command not found\n";
+	}
+}
+
+string getCommand(){
 	string input;
+	cout << "\n$ ";
 	getline(cin, input);
-	cout << input << ": command not found\n";
+	return input;
+}
+
+string cleanCommand( string& str ){
+	istringstream iss(str);
+    string firstWord;
+    iss >> firstWord;
+    return firstWord;
+}
+
+string getCode(string& str){
+	istringstream iss(str);
+	string firstWord, secondWord;
+	iss >> firstWord >> secondWord;
+	return secondWord;
+}
+
+
+
+// Utils
+int stringToInt(string numString){
+	istringstream iss(numString);
+	int numInt;
+	iss >> numInt;
+    return numInt;
 }
