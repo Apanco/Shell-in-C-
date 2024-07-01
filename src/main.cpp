f1767cf48789892237bcb0663896b04bb375e10c
David Apanco <dperezapanco@gmail.com>
2024-07-01 00:33:23 -0600

    unknow command

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,25 @@
 #include <iostream>
 using namespace std;
-int main() {
-  // Flush after every std::cout / std:cerr
-  std::cout << std::unitbuf;
-  std::cerr << std::unitbuf;
 
-  // Uncomment this block to pass the first stage
-  std::cout << "$ ";
-  //
-  std::string input;
-  std::getline(std::cin, input);
+string getComand();
+
+int main() {
+	
+	string notFound = "command not found";
+	
+	string input;
+	// Flush after every std::cout / std:cerr
+	std::cout << std::unitbuf;
+	std::cerr << std::unitbuf;
+	
+	// Uncomment this block to pass the first stage
+	input = getComand();
+	cout<<"\n"<<input<<": "<<notFound;
 }
+string getComand(){
+	string input;
+	cout<<"$ ";cin>>input;
+	return input;
+}
+
+
